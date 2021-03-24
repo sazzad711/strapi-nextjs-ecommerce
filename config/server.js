100ddf1d4ee9b9f8cd9385a4c8d271b4020283e5
Sazzad Hossain <somoy711@gmail.com>
2021-03-24 18:09:44 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'afc1b55c849bce71dc9299329da42e24'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3e571030-e323-46fd-bf0a-54061b6c6870'
+};
