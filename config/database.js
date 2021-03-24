660fec25dfeb6f53e19255f12f91d4c5a6e441b5
Sazzad Hossain <somoy711@gmail.com>
2021-03-24 23:27:26 +0100

    delete database

--- config/database.js
+++ config/database.js
@@ -31,18 +31,6 @@ module.exports = ({ env }) =>{
    }
 
    return {
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-}
+
+      }
 };
